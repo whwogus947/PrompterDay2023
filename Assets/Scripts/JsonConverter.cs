ebfc41ae08945d683261cce215e16ee0aee25f95
yejin <kimyejin0044@gmail.com>
2023-08-31 23:32:14 +0900

    json convert

--- Assets/Scripts/JsonConverter.cs
+++ Assets/Scripts/JsonConverter.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Firebase;
+using Firebase.Firestore;
+using Firebase.Extensions;
 
 public class JsonConverter : MonoBehaviour
 {
     void Start()
     {
-        
+        // DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
     public static GPTResult TestAsSample()
@@ -20,10 +23,19 @@ public class JsonConverter : MonoBehaviour
 
     public void ConvertToJson()
     {
-        string jsonFilePath = "city"; // Without the file extension
+        // string jsonFilePath = "city"; // Without the file extension
+        // TextAsset jsonTextAsset = Resources.Load<TextAsset>(jsonFilePath);
+        // string jsonText = jsonTextAsset.text;
+        // Debug.Log(Convert(jsonText));
+
+        string jsonFilePath = "bakery"; // Without the file extension
         TextAsset jsonTextAsset = Resources.Load<TextAsset>(jsonFilePath);
         string jsonText = jsonTextAsset.text;
         Debug.Log(Convert(jsonText));
+        // mDatabaseRef.Child("users").Child(userId).SetRawJsonValueAsync(json);
+
+
+
     }
 
 
@@ -31,12 +43,12 @@ public class JsonConverter : MonoBehaviour
     {
         if (string.IsNullOrEmpty(json))
             return null;
-
-        GPTResult result = JsonUtility.FromJson<GPTResult>(json);
         
-        //Debug.Log(result.title);
-        //Debug.Log(result.item.Length);
-        //Debug.Log(result.conclusion);
+        GPTResult result = JsonUtility.FromJson<GPTResult>(json);
+
+        // Debug.Log(result.meetingTopic);
+        // Debug.Log(result.meetingAgenda);
+        // Debug.Log(result.conclusion);
 
         return result;
     }
@@ -44,23 +56,43 @@ public class JsonConverter : MonoBehaviour
     [System.Serializable]
     public class GPTResult
     {
-        public string title;
-        public Item[] item;
+        public string meetingTopic;
+        public MeetingAgenda[] meetingAgenda;
+        public int selectedIdea;
         public string conclusion;
+        public List<string> topicForFurtherDiscussion;
     }
 
     [System.Serializable]
-    public class Item
+    public class MeetingAgenda
     {
+        public int numberOfIdea;
+        public string summaryOfIdea;
         public string speaker;
-        public string message;
-        public string time;
-
-        public Item(string speaker, string message, string time)
+        public string contentsOfIdea;
+        public Comments[] comments;
+    
+        public MeetingAgenda(int numberOfIdea, string summaryOfIdea, string speaker, string contentsOfIdea, Comments[] comments)
         {
+            this.numberOfIdea = numberOfIdea;
+            this.summaryOfIdea = summaryOfIdea;
             this.speaker = speaker;
-            this.message = message;
-            this.time = time;
+            this.contentsOfIdea = contentsOfIdea;
+            this.comments = comments;
         }
     }
+    [System.Serializable]
+    public class Comments
+    {
+        public string speaker;
+        public string contentsOfComment;
+        public bool isPositive;
+
+        public Comments(string speaker, string contentsOfComment, bool isPositive){
+            this.speaker = speaker;
+            this.contentsOfComment = contentsOfComment;
+            this.isPositive = isPositive;
+        }
+    }
+
 }
